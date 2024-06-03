a008b72083b5d7c781bc35708c50b59754adcdc7
Alexeypropuskov <alexmagcikstile@gmail.com>
2024-06-03 15:47:15 +0300

    visual studio + playercontroller

--- Assets/Prefabs/PlayerController.cs
+++ Assets/Prefabs/PlayerController.cs
@@ -1,11 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
+    
+    public int Jumpforce;
+    public Rigidbody2D rb;
+    public Vector2 moveVector;
+    public float speed = 2f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
     private void Update()
     {
-        transform.position += new Vector3(1,0,0) * Input.GetAxis("Horizontal");
+        walk();
+        
     }
+
+    void walk()
+    {
+        moveVector.x = Input.GetAxis("Horizontal");
+        ///rb.velocity = new Vector2(moveVector.x * speed, rb.velocity.y);
+        rb.AddForce(moveVector * speed);
+    }
+
+
 }
